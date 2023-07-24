73e053fd3a425d97e5c5e0d844a93518c3c8006b
Satendra singh <satendra.panwar@aspiresys.com>
2023-07-24 19:16:41 +0530

    created Employee form with vailidations

--- src/app/forms/employee-form/employee-form.component.ts
+++ src/app/forms/employee-form/employee-form.component.ts
@@ -6,44 +6,40 @@ import { controlNames } from './employee.constant';
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
-  styleUrls: ['./employee-form.component.css']
+  styleUrls: ['./employee-form.component.css'],
 })
 export class EmployeeFormComponent implements OnInit {
-
   employeeForm!: FormGroup;
-  isvaild= false
+  isvaild = false;
   controlNames = controlNames;
-    
-  constructor(
-    private fb: FormBuilder,
-    private toastr: ToastrService
-  ){  }
 
-
-
+  constructor(private fb: FormBuilder, private toastr: ToastrService) {}
 
   ngOnInit(): void {
     this.employeeForm = new FormGroup({
-      employees: this.fb.array([this.newEmployee()])
+      employees: this.fb.array([this.newEmployee()]),
     });
   }
 
   employees(): FormArray {
     return this.employeeForm.get('employees') as FormArray;
   }
-  get getemployees(): FormArray {
-    return this.employeeForm.get('employees') as FormArray;
-  } 
 
   newEmployee(): FormGroup {
     return this.fb.group({
-      firstName: ['',Validators.required],
-      lastName: ['',Validators.required],
-      email: ['',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
-      age: ['',Validators.required],
-      gender: ['',Validators.required],
-      address: ['',Validators.required],
-      phoneNumberList: this.fb.array([this.newPhoneNumber()])
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ],
+      ],
+      age: ['', Validators.required],
+      gender: ['', Validators.required],
+      address: ['', Validators.required],
+      phoneNumberList: this.fb.array([this.newPhoneNumber()]),
     });
   }
 
@@ -52,14 +48,19 @@ export class EmployeeFormComponent implements OnInit {
   }
 
   employeePhoneNumber(i: number): FormArray {
-    return this.employees()
-      .at(i)
-      .get('phoneNumberList') as FormArray;
+    return this.employees().at(i).get('phoneNumberList') as FormArray;
   }
 
   newPhoneNumber(): FormGroup {
     return this.fb.group({
-      phoneNumber: ['',[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),Validators.maxLength(10)]],
+      phoneNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+          Validators.maxLength(10),
+        ],
+      ],
     });
   }
 
@@ -68,11 +69,12 @@ export class EmployeeFormComponent implements OnInit {
   }
 
   onSubmit() {
- 
-    console.log(this.employeeForm);
+    if (!this.employeeForm.valid) {
+      this.toastr.error('Invalid Form');
+    } else {
+      this.isvaild = true;
+      this.toastr.success('Created Successfully');
+      console.log(this.employeeForm);
+    }
   }
-
-
-
-
 }
